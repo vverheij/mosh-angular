f9c45f0eed5d316a824853b26f7b7544ebbf23cc
Victor <victor@atom.nl>
2018-09-19 10:28:45 +0200

    finished excercise

---
+++ src/app/zippy/zippy.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'zippy',
+  templateUrl: './zippy.component.html',
+  styleUrls: ['./zippy.component.css']
+})
+export class ZippyComponent {
+  @Input('title') title: string;
+  isExpanded: boolean = true;
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
+}
